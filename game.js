02be8ffd496d3909e1f5492966e83b8bb30b24c7
Akira <86209025+AbraaoGeraldo@users.noreply.github.com>
2024-07-15 18:55:01 -0300

    Making the human prompt and score variables

--- game.js
+++ game.js
@@ -2,16 +2,31 @@ console.log("Hello World")
 
 function getComputerChoice () {
     let pc = Math.random()
-     if (pc > 0.1 < 0.5) {
+     if (pc > 0.1 < 0.3) {
         console.log("Pedra")
-    } else if (pc > 0.6 < 0.7 ) {
+    } else if (pc > 0.4 < 0.6 ) {
         console.log("Papel")
-    } else if (pc > 0.8 == 1) {
+    } else if (pc > 0.7 == 1) {
         console.log("Tesoura")
     }
 
     return 
         
 }
+
+function getHumanChoice() {
+    let escolha = prompt("Choose your weapon")
+}
+
+
+let humanScore =  0
+let computerScore = 0
+
+
+
+
+
+console.loggetHumanChoice()
+
 
 console.log(getComputerChoice())

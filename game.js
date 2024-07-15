0a4ab61a85d63b40ff76123192e78bd8cab8a76a
Akira <86209025+AbraaoGeraldo@users.noreply.github.com>
2024-07-15 18:32:57 -0300

    Doing the function for computer choices!

---
+++ game.js
@@ -0,0 +1,17 @@
+console.log("Hello World")
+
+function getComputerChoice () {
+    let pc = Math.random()
+     if (pc > 0.1 < 0.5) {
+        console.log("Pedra")
+    } else if (pc > 0.6 < 0.7 ) {
+        console.log("Papel")
+    } else if (pc > 0.8 == 1) {
+        console.log("Tesoura")
+    }
+
+    return 
+        
+}
+
+console.log(getComputerChoice())

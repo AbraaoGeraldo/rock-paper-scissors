3a803e91a6b1a4130cfe0bc30f8250dea58fb371
Akira <akiray165@gmail.com>
2025-10-14 19:19:10 -0300

    Corrigindo o jogo, adicionei um novo arquivo .js como forma de melhor organização

---
+++ app.js
@@ -0,0 +1,17 @@
+playerScore =1;
+cpuSccore = 1;
+
+function getComputerChoice() {
+    let cpu = parseInt(Math.random() * 10 + 1);
+    let cpuChoice = '';
+
+    if (cpu > 5) {
+        console.log('Tesoura');
+    } else if (cpu < 5) {
+        console.log('Pedra');
+    } else {
+        console.log('Papel')
+    }
+}
+
+getComputerChoice()

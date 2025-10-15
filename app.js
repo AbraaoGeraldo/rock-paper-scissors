baa0e4edba9e9e0fe3b041a10ea209e41fbb98a4
Akira <akiray165@gmail.com>
2025-10-15 14:18:04 -0300

    Making the human choice function and new variables

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 let playerScore =1;
 let cpuSccore = 1;
 
+let rock = 'Rock';
+let paper = 'paper';
+let scissors = 'Scissors'
+
 function getComputerChoice() {
     let cpu = parseInt(Math.random() * 10 + 1);
     let cpuChoice = '';
 
     if (cpu > 5) {
-        cpuChoice = 'Scissors';
+        cpuChoice = scissors;
     } else if (cpu < 5) {
-        cpuChoice = 'Rock';
+        cpuChoice = rock;
     } else {
-        cpuChoice = "Paper";
+        cpuChoice = paper;
+    }
+
+    return cpuChoice;
+}
+
+function getHumanChoice() {
+    let playerChoice = prompt('Choose your weapon');
+
+    if (playerChoice == "Rock", "rock", "ROCK", "RocK", "paper", "Paper", "PapeR", "PAPER", "Scissors", "scissors", "ScissorS", "SCISSORS") {
+        return playerChoice;
     }
 }
 
 getComputerChoice();
+getHumanChoice()
